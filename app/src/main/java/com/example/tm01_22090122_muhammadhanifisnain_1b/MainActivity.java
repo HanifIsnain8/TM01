4c4c4f50f094a1a3d06b317b5e59d3e73e8ffc7a
hanifisnain8 <hanifisnain8@gmail.com>
2023-03-23 14:05:34 +0700

    ok

--- app/src/main/java/com/example/tm01_22090122_muhammadhanifisnain_1b/MainActivity.java
+++ app/src/main/java/com/example/tm01_22090122_muhammadhanifisnain_1b/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.tm01_22090122_muhammadhanifisnain_1b;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
